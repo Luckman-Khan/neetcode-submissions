573b45bb03375c35b65d311942a0a169f91ae78c
Lukman Khan <luckmankhan005@gmail.com>
2026-07-31 14:36:16 +0530

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        clean_s = ""
+
+        for c in s:
+
+            if c.isalnum():
+                clean_s+=c.lower()
+            
+        return clean_s == clean_s[::-1]
