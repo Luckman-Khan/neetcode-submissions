1717bffece2b4cf2296eb45d3b9868c8cf053b46
Lukman Khan <luckmankhan005@gmail.com>
2026-08-01 21:15:20 +0530

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {')':'(', ']':'[', '}':'{'}
+
+        for ch in s:
+
+            if ch in mapping:
+
+                if stack and mapping[ch] == stack[-1]:
+                    stack.pop()
+                
+                else:
+                    return False
+            else:
+                stack.append(ch)
+        return not stack
