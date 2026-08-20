9fdfdb7a2deb3db789bcebb4a208219d7a2d228b
Lukman Khan <luckmankhan005@gmail.com>
2026-08-20 12:10:55 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        def fib(n):
+            
+            a = 0
+            b= 1
+
+            for _ in range(n+1):
+                a,b = b,a+b
+        
+            return a
+        return fib(n)
